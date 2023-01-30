9f33cedd6c6c74499b0e3a91342f1a050a2cb793
zhangxingzhi <zhangxingzhi@microsoft.com>
2023-01-30 16:47:22 +0800

    refactor: refactor json schema test

--- sdk/ml/azure-ai-ml/tests/pipeline_job/unittests/test_private_preview_disabled.py
+++ sdk/ml/azure-ai-ml/tests/pipeline_job/unittests/test_private_preview_disabled.py
@@ -2,7 +2,9 @@ from pathlib import Path
 import pytest
 import yaml
 from jsonschema.validators import validate
-from azure.ai.ml.entities import PipelineJob
+from marshmallow import Schema
+
+from azure.ai.ml.entities import PipelineJob, CommandComponent
 from test_utilities.json_schema import PatchedJSONSchema
 
 from .._util import _PIPELINE_JOB_TIMEOUT_SECOND
@@ -14,13 +16,27 @@ from .._util import _PIPELINE_JOB_TIMEOUT_SECOND
 @pytest.mark.unittest
 @pytest.mark.pipeline_test
 class TestPrivatePreviewDisabled:
-    def test_public_json_schema(self):
+    @pytest.mark.parametrize(
+        "source_file,target_schema",
+        [
+            pytest.param(
+                "command_component.yaml",
+                CommandComponent._create_schema_for_validation(context={"base_path": "./"}),
+                id="command_component",
+            ),
+            pytest.param(
+                "pipeline_job.yaml",
+                PipelineJob._create_schema_for_validation(context={"base_path": "./"}),
+                id="pipeline_job",
+            ),
+        ],
+    )
+    def test_public_json_schema(self, source_file: str, target_schema: Schema):
         # public json schema is the json schema to be saved in
         # https://azuremlschemas.azureedge.net/latest/pipelineJob.schema.json
-        base_dir = Path("./tests/test_configs/pipeline_jobs/json_schema_validation")
-        target_schema = PatchedJSONSchema().dump(PipelineJob._create_schema_for_validation(context={"base_path": "./"}))
+        target_schema = PatchedJSONSchema().dump(target_schema)
 
-        with open(base_dir.joinpath("component_spec.yaml"), "r") as f:
+        with open(Path("./tests/test_configs/json_schema_validation").joinpath(source_file), "r") as f:
             yaml_data = yaml.safe_load(f.read())
 
         validate(yaml_data, target_schema)
